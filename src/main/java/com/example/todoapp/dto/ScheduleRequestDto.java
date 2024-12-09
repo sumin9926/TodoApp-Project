e4f7ec6ac5cdfc7452a7652d17dd31dc395d0c25
임PRO스터\su992 <su9926@naver.com>
2024-12-09 17:58:26 +0900

    :hammer: update Date Format

--- src/main/java/com/example/todoapp/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/todoapp/dto/ScheduleRequestDto.java
@@ -1,9 +1,15 @@
 package com.example.todoapp.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Getter
 public class ScheduleRequestDto {
     private String name;
     private String details;
     private String password;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate updatedDate;
 }
